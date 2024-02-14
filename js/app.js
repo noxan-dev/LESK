a6d2557baa8c5ab0205ddb6d83d572e9c517d5f9
Chaim Malek <chaimmalek@gmail.com>
2024-02-14 16:34:16 -0500

    Cleaned up menu

--- js/app.js
+++ js/app.js
@@ -4,11 +4,12 @@ const dips = $('#dips')
 $.getJSON('https://raw.githubusercontent.com/noxan-dev/LESK/master/menu.json?token=GHSAT0AAAAAABULC6SXOFRAEJLPIWSQCPAEZNPAAKQ', function (data) {
   $.each(data, function (key, val) {
     dips.append(
-      `<div class="max-w-sm rounded overflow-hidden shadow-lg p-4 bg-white">
+      `<div class="max-w-sm rounded overflow-hidden shadow-lg p-2 mx-4 md:m-0 bg-white">
           <img class="w-full" src="" alt="">
-          <p>Name: ${val.name}</p>
-          <p>About: ${val.description}</p>
+          <p><span>Name:</span> ${val.name}</p>
+          <p class="flex gap-x-1"><span>About:</span> ${val.description}</p>
           <p>Price: ${val.price}</p>
+          <p>Size: ${val.size}</p>
       </div>`);
   })
 })
